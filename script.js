dc0f126f2cbd69fc08464c17e60f7f3f23182df3
FaryalKhwaja <iamfaryalkhwaja@gmail.com>
2025-07-24 23:21:38 +0500

    Change pictures and make buttons work

---
+++ script.js
@@ -0,0 +1,9 @@
+let download = document.querySelector(".sign_up");
+download.addEventListener("click", () => {
+    window.open("https://www.minecraft.net/en-us/download", "_blank");
+});
+
+let play = document.querySelector(".button");
+play.addEventListener("click", () => {
+    window.open("https://www.minecraft.net/en-us/article/how-minecraft", "_blank");
+});
